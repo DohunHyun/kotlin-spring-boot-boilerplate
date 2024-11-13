23b21230793769a8c0763aebb2fe69bef705c156
DohunHyun <dohun.hyun@gmail.com>
2024-11-13 14:31:34 +0900

    initial set project

---
+++ src/main/kotlin/com/group/kotlin_spring_boot_boilerplate/KotlinSpringBootBoilerplateApplication.kt
@@ -0,0 +1,11 @@
+package com.group.kotlin_spring_boot_boilerplate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBootBoilerplateApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringBootBoilerplateApplication>(*args)
+}
